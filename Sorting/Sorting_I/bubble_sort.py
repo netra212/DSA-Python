187a85d04e1d55b201e0a2238bdc24fef7faac96
netr212 <netra200021kcbdr@gmail.com>
2025-01-06 20:13:42 +0000

    sorting algorithm

---
+++ Sorting/Sorting_I/bubble_sort.py
@@ -0,0 +1,9 @@
+# Push the maximum element to the last by adjacent swaps.
+#
+
+def bubble_sort(arr, n):
+    pass
+
+if __name__ == "__main__":
+    print()
+
---
+++ Sorting/Sorting_I/selection_sort.py
@@ -0,0 +1,11 @@
+# selection sort: takes the minimum elements at the front.
+
+
+def selection_sort(arr, n):
+    for i in range(0, n-2):
+        continue
+
+
+if __name__ == "__main__":
+    arr = [14, 46, 23, 7, 9, 16]
+    selection_sort(arr, len(arr))
