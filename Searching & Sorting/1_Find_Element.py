3e3148b22a8ddf680d0b94b3ade7595c73b30800
netra212 <netra200021kcbdr@gmail.com>
2025-11-06 06:10:57 +0000

    binary search

--- Searching & Sorting/1_Find_Element.py
+++ Searching & Sorting/1_Find_Element.py
@@ -1,7 +1,18 @@
 
 # numpy and array. 
 
+# [10, 23, 45, 70, 11]
+# return the target index if present. 
+my_list = [10, 23, 45, 70, 11]
+target = 70
 
+def linear_search(my_list, target):
+    size = len(my_list)
+    for index in range(0, size):
+        if (target == my_list[index]):
+            return index
+    return -1
 
+result = linear_search(my_list, target)
 
 
---
+++ Searching & Sorting/Binary_Search/1_Binary_Search.py
@@ -0,0 +1,10 @@
+# Binary Search: Only apply on the sorted List or Arrays. 
+# [10, 23, 45, 50, 70, 85]
+
+# STEPS:
+
+# 1st step: Consider the full array or list. 
+# start = 0, end = n - 1
+
+# 2nd step: see the middle element. 
+# 
