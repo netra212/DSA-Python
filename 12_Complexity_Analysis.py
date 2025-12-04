28a12bd73b63fd192e4729797d3284248c8fb861
netra212 <netra200021kcbdr@gmail.com>
2025-12-04 16:09:38 +0000

    Complexity Analysis

---
+++ 12_Complexity_Analysis.py
@@ -0,0 +1,20 @@
+'''
+# Docstring for 12_Complexity_Analysis
+
+- Complexity - A factor involved in complicated process or situation. 
+
+# Time Complexity:
+- Can you tell the time complexity of code that you have written. 
+- Can you improve the Complexity. 
+
+# ----- Asymptotic notation -----
+
+- Main idea of our analysis is to do have a measure of the efficiency of algorithm that don't depend on. 
+- machine specific constants. 
+- require algorithm to be implemented. 
+
+- Asymptotic notation are mathematic tools to represent time complexity of our algorithm. 
+
+'''
+
+
