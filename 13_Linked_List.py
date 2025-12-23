39c4eaef3f9f550f133ae38178383cfe99f6bf99
netra212 <netra200021kcbdr@gmail.com>
2025-12-23 18:57:51 +0000

    Linked List

--- 13_Linked_List.py
+++ 13_Linked_List.py
@@ -120,24 +120,65 @@ def insert_At_Head_LL(head, data):
 head = insert_At_Head_LL(head, 100)
 
 def insert_at_tail_ll(head, data):
-    
+
     temp = head
 
     newNode = Node(data)
 
     # If LinkedList is empty then just return the newNode only or newNode becomes the head. 
-    if(head == None):
+    if(head is None):
         return newNode
     
-    if(temp.next == None):
-        temp.next = newNode
-        newNode.next = None
+    while(temp.next != None):
+        temp = temp.next
+    
+    temp.next = newNode
 
+    return head
 
+def insert_At_Tail_Recursively(head, data):
+    temp = head
+    newNode = Node(data)
+    # 1. Base case. 
+    if(head is None):
+        return newNode
 
+    # 3. Our work.
+    if(head.next == None): # we are at the tail. 
+        head.next = newNode
+        return head
 
+    # 2. Recursion call. 
+    head.next = insert_At_Tail_Recursively(head.next, data)
 
+# Insert a node at an Index. 
+def insert_Node_at_an_Index(head, data, index):
+    '''
+    Docstring for insert_Node_at_an_Index
+    
+    1. Make a temp. 
+    2. move temp till index 1, using count var. 
+    3. update the connection to insert new node. 
+    4. Order is very important. 
+    '''
+    temp = head
+    newNode = Node(data)
+    count = 0
 
+    if(index == 0):
+        return insert_At_Head_LL(head, data)
+    
+    while temp is not None and count < index - 1:
+        temp = temp.next
+        count += 1
+
+    newNode.next = temp.next # preserving the previous connection so that overall connection does not get lost. 
+
+    temp.next = newNode
+    
+    return head
+
+# Insert at Index with Recursion. 
 
 
 
