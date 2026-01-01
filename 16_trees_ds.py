d2e25942588791e16a210cae47bc2cf50bbe7ad3
netra212 <netra200021kcbdr@gmail.com>
2026-01-01 21:59:11 +0000

    trees data structure

---
+++ 16_trees_ds.py
@@ -0,0 +1,19 @@
+'''
+# Docstring for 16_trees_ds
+- Non-Linear Data Structure.
+- Tree is a Heirarchial data structure consisting of nodes which are connected by edges.
+
+1. Node - Each item of my tree. 
+2. Edges - Connnection between 2 nodes. 
+3. Root - Topmost node. 
+4. Leaf - Node with no-childrens. 
+5. Parent - 
+6. Child - 
+7. Ancestors - 
+8. Descendants - 
+9. Subtree - 
+10. Level - 
+'''
+
+
+
