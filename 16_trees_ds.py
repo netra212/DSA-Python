c203c65c3e6f87aab864d35faf9ae0a982b85454
netra212 <netra200021kcbdr@gmail.com>
2026-01-02 00:29:34 +0000

    feat(trees with recursion)

--- 16_trees_ds.py
+++ 16_trees_ds.py
@@ -12,8 +12,35 @@
 7. Ancestors - 
 8. Descendants - 
 9. Subtree - 
-10. Level - 
+10. Level/Depth - 
+11. Path - Sequence of nodes and edges connecting one node to another. 
 '''
 
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+root = TreeNode(1)
+
+child1 = TreeNode(2)
+child2 = TreeNode(3)
+child3 = TreeNode(4)
+
+root.children.append(child1)
+root.children.append(child2)
+root.children.append(child3)
+
+print(root.children[0].data)
+
+# Print tree using Recursion. 
+def print_tree(root):
+    
+    print(root.data)
+
+    for eachChild in root.children:
+        print_tree(eachChild)
+
+print_tree(root)
 
 
